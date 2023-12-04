629ab6b9a50e781a746e6a3e2bdcaf4aa6925431
philipp.schopp@iav.de <pschopp@iavgroup.local>
2023-12-04 07:12:34 +0100

    solved puzzle 4

--- 04/04.py
+++ 04/04.py
@@ -1,5 +1,32 @@
+from pathlib import Path
+import re
+
 def do_main():
-    return
+    with open(Path('input.txt')) as file:
+        lines = [line.rstrip() for line in file]
+
+    point_sum = 0
+    cards = {i: 1 for i in range(len(lines))}
+
+    for line_index, line in enumerate(lines):
+        match = re.search(r'([\d\s]+)\s\|\s([\d\s]+)', line)
+        numbers_before = list(map(int, match.group(1).split()))
+        numbers_after = list(map(int, match.group(2).split()))
+
+        copy_index = 1
+        points = 0
+        for num in numbers_after:
+            if num in numbers_before:
+                cards[line_index+copy_index] += (1 * cards[line_index])
+                copy_index += 1
+                if points == 0:
+                    points = 1
+                    continue
+                points = points * 2
+        point_sum += points
+
+    print(point_sum)
+    print(sum(cards.values()))
 
 if __name__ == '__main__':
     do_main()
